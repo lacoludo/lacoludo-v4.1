af8ed2ed1293c25900ab7ab4b377b65f5b9ede59
Ludovic Lacouture <lacoludo@outlook.com>
2020-11-20 04:03:55 +0100

    Pages generated by local JSON

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -3,32 +3,25 @@ import { GetStaticPaths, GetStaticProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
-import kebabCase from 'lodash.kebabcase'
-import cn from 'classnames'
+// import cn from 'classnames'
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
+import { readProjects, readProject } from '../../shared/api/slug.api'
 import FadeOnScroll from '../../shared/utils/fade-on-scroll.util'
 import Header from '../../shared/components/header.component'
 import MenuDesktop from '../../shared/components/menu-desktop.component'
 import MenuMobile from '../../shared/components/menu-mobile.component'
 import Footer from '../../shared/components/footer.component'
 import ScrollToTop from '../../shared/components/scroll-to-top.component'
-import Data from '../../projects/constants/data.constant'
-import Technologies from '../../projects/constants/technologies.constant'
 
 export const getStaticPaths: GetStaticPaths = async () => ({
-  paths: Data.map(posts => ({
-    params: {
-      slug: kebabCase(posts.name)
-    }
-  })),
+  paths: await readProjects(),
   fallback: false
 })
 
 export const getStaticProps: GetStaticProps = async ({ params }) => ({
   props: {
-    project: Data.filter(data => kebabCase(data.name) === params?.slug)[0]
+    project: await readProject(params)
   }
 })
 
@@ -43,8 +36,6 @@ const Project = ({ project }: any) => {
     fontSize: '15px',
     marginBottom: '-3px'
   }
-
-  console.log(project)
 
   return (
     <>
@@ -65,8 +56,12 @@ const Project = ({ project }: any) => {
             <div className='title'>
               <h3>{project.name}</h3>
               <ul className='list'>
-                {project.technologies.map((technology: any, index: any) => (
-                  <li key={index} onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
+                {/* {project.technologies.map((technology: any, index: any) => (
+                  <li
+                    key={index}
+                    onMouseEnter={toggleHover}
+                    onMouseLeave={toggleHover}
+                  >
                     <i
                       className={cn(`devicon-${technology.icon}`, {
                         colored: hover
@@ -74,7 +69,7 @@ const Project = ({ project }: any) => {
                     ></i>{' '}
                     <strong>{technology.name}</strong>
                   </li>
-                ))}
+                ))} */}
               </ul>
             </div>
           </div>
